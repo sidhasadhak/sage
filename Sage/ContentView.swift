05951120f9786fcbb0aa640730fd048224e03505
sidhasadhak <sidhasadhak@outlook.com>
2026-04-22 00:59:00 +0200

    Initial Commit

---
+++ Sage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sage
+//
+//  Created by Amit Kamlapure on 22.04.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sage/SageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SageApp.swift
+//  Sage
+//
+//  Created by Amit Kamlapure on 22.04.26.
+//
+
+import SwiftUI
+
+@main
+struct SageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
